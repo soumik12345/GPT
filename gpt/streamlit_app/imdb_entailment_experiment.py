5475e0e6d28364fb16120d448a504f14254de022
soumik12345 <19soumik.rakshit96@gmail.com>
2020-11-03 14:16:35 +0530

    Completed IMDB Text Entailment App Module

--- gpt/streamlit_app/imdb_entailment_experiment.py
+++ gpt/streamlit_app/imdb_entailment_experiment.py
@@ -5,6 +5,7 @@ def imdb_text_entailment_app_module():
     from gpt.experiments.language_model import IMDBReviewLanguageExperiment
     experiment = IMDBReviewLanguageExperiment()
     initialize_wandb = st.checkbox('Initialize Wandb')
+
     if initialize_wandb:
         project_name = st.text_input('Wandb Project Name', 'gpt')
         experiment_name = st.text_input('Experiemnt Name', 'imdb_language_model')
@@ -17,6 +18,26 @@ def imdb_text_entailment_app_module():
             wandb_api_key=wandb_api_key
         )
         st.text_input('Done!!!')
+
     st.text('Fetching and Building IMDB Large Movie Review Dataset...')
     experiment.build_dataset('https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz')
+    st.text('Done!!!')
+
+    start_text = st.text_input('Enter a start token to be infered upon during training', 'the actor was')
+    st.text('Tokenizing Start Tokens...')
+    start_tokens = experiment.tokenize(start_text=start_text)
+    st.text('Done!!!')
+
+    max_length = st.number_input('Enter max sequence length', 100)
+    max_tokens = st.number_input('Enter max tokens to be predicted', 40)
+    top_k = st.number_input('Enter number of top tokens to be sampled', 10)
+    epochs = st.number_input('Enter number of epochs', 30)
+
+    st.text('Training...')
+    experiment.train(
+        epochs=epochs, start_tokens=start_tokens,
+        max_length=max_length, max_tokens=max_tokens,
+        top_k=top_k, infer_every=1,
+        log_on_wandb=True if initialize_wandb else False
+    )
     st.text('Done!!!')
